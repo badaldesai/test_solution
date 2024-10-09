46c579e2a0c17bec6ad6da7c50515f6319b8bcb8
Badal Desai <badal@geolava.com>
2024-10-08 21:04:09 -0400

    Solution

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
-const pooledDownload = (connect, save, downloadList, maxConcurrency) => {
-  return connect().then((connection) => {
-    const { download, close } = connection
-    return download(downloadList[0]).then((result) => save(result)) // download the first file and save the result
-  })
+const pooledDownload = async (connect, save, downloadList, maxConcurrency) => {
+  let activeDownloads = 0;
+  let downloadIndex = 0;
+  let connectionFailed = false;
+  const downloadFile = async (connection, url) => {
+    const { download } = connection;
+    try {
+      const result = await download(url);
+      await save(result);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  const startDownload = async () => {
+    while (downloadIndex < downloadList.length && !connectionFailed) {
+      let connection;
+      if (activeDownloads >= maxConcurrency) {
+        await new Promise((resolve) => setTimeout(resolve, 100));
+        continue;
+      }
+      try {
+        connection = await connect();
+      } catch (error) {
+        connectionFailed = true;
+        throw new Error('connection failed');
+      }
+      const url = downloadList[downloadIndex];
+      downloadIndex++;
+      activeDownloads++;
+      await downloadFile(connection, url);
+      await connection.close();
+      activeDownloads--;
+    }
+  }
+  const workers = Array.from({ length: maxConcurrency }, startDownload);
+
+  try {
+    await Promise.all(workers);
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = pooledDownload
